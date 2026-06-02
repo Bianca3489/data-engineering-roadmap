53d566a4e10a2e13010956b823e00c6f18bff4d5
Bianca3489 <bia.rscr@hotmail.com>
2026-06-02 11:59:20 -0300

    exercicios python

---
+++ Python/modulos/modulos_ex.py
@@ -0,0 +1,24 @@
+# from math import sqrt , ceil , floor, trunc
+
+# num = float(input("Digite um numero: "))
+# print(sqrt(num))
+# print(ceil(num))
+# print(floor(num))
+# print(trunc(num))
+
+# import random
+# lista = [1, 2, 3, 4, 5]
+
+# print(random.choice(lista))
+# print(random.randint(1, 10))
+# print(random.uniform(1, 10))
+
+# from datetime import datetime
+
+# print(datetime.now())
+# print(datetime.today())
+
+# pyrefly: ignore [missing-import]
+import emoji
+print(emoji.emojize('Python é :thumbs_up:'))
+print(emoji.demojize('Python é :thumbs_up:'))
