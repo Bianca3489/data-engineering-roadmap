578175bcecfcc71722296a93ff1ed822dd1361ab
Bianca3489 <bia.rscr@hotmail.com>
2026-06-17 19:50:31 -0300

    conceito tuplas e exercicios

---
+++ Python/tuplas/conceito_tuplas.py
@@ -0,0 +1,34 @@
+# Tuplas
+
+# Conceito de Tuplas
+
+# Tuplas são coleções ordenadas, ou seja, os elementos têm uma posição
+# Exemplo:
+
+# Tuplas são imutáveis, ou seja, não podem ser alteradas
+
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 55, 32, 25, 55.5, 'a', True)
+print(len(lanche))
+# Tuplas são ordenadas, ou seja, os elementos têm uma posição
+print(lanche[0]) # Imprime o primeiro elemento
+print(lanche[1]) # Imprime o segundo elemento
+print(lanche[2]) # Imprime o terceiro elemento
+print(lanche[3]) # Imprime o quarto elemento
+print(lanche[4]) # Imprime o quinto elemento
+print(lanche[5]) # Imprime o sexto elemento
+print(lanche[6]) # Imprime o sétimo elemento
+print(lanche[7]) # Imprime o oitavo elemento
+print(lanche[8]) # Imprime o nono elemento
+print(lanche[9]) # Imprime o décimo elemento
+print(lanche[-1]) # Imprime o último elemento
+print(lanche[-2]) # Imprime o penúltimo elemento
+print(lanche[-3]) # Imprime o antepenúltimo elemento
+print(lanche[-4]) # Imprime o anteantepenúltimo elemento
+print(lanche[-5]) # Imprime o anteantantepenúltimo elemento
+print(lanche[-6]) # Imprime o anteantantepenúltimo elemento
+print(lanche[-7]) # Imprime o anteantantepenúltimo elemento
+print(lanche[-8]) # Imprime o anteantantepenúltimo elemento
+print(lanche[-9]) # Imprime o anteantantepenúltimo elemento
+print(lanche[-10]) # Imprime o anteantantepenúltimo elemento
+
+print(len(lanche)) # Imprime o tamanho da tupla
