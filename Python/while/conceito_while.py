6e6038dd73c28f9a475c098e9e1bd496965dca44
Bianca3489 <bia.rscr@hotmail.com>
2026-06-21 13:56:20 -0300

    while exercicios

--- Python/while/conceito_while.py
+++ Python/while/conceito_while.py
@@ -13,7 +13,35 @@
 # -----------------------------------------------------------
 # -----------------------------------------
 
-i = 0
-while i < 5:
-    print("Contagem: ", i)
-    i += 1
+# i = 0
+# while i < 5:
+#     print("Contagem: ", i)
+#     i += 1
+
+
+# Exemplo 1 : Contagem 1 a 9 com while e com For
+
+# for c in range(1, 10):
+#     print(c)
+# print("Fim do loop")
+
+# c =1
+# while c < 10:
+#     print(c)
+#     c = c + 1
+#     print('Fim')
+
+# Exemplo 2: Tabuada while e for
+
+# tabuada = int(input("Digite um número : "))
+# multiplicador = 1
+# while multiplicador <= 10:
+#     print(f"{tabuada} x {multiplicador} = {tabuada} * {multiplicador} ")
+#     multiplicador = multiplicador + 1
+# print("Fim do Loop")
+
+# tabuada = int(input("Digite um número : "))
+# for i in range(1,11):
+#     print(f"{tabuada} x {i} = {tabuada * i}")
+# print("Fim do Loop")
+
--- Python/while/exercicios_while.py
+++ Python/while/exercicios_while.py
@@ -3,29 +3,54 @@
 #1. Crie um programa que receba números positivos do usuário e continue pedindo até que um número negativo seja inserido. Ao final, imprima a soma de todos os números positivos inseridos.
 
 
-soma = 0
+# soma = 0
 
-while True:
-    num = int(input("Digite um numero : "))
-    if num < 0:
-        break
-    soma = soma + num
+# while True:
+#     num = int(input("Digite um numero : "))
+#     if num < 0:
+#         break
+#     soma = soma + num
 
-print(f"A soma dos numeros é {soma}")
+# print(f"A soma dos numeros é {soma}")
 
 # 2) Um usuário vai inserir notas de alunos. O programa deve parar quando o usuário digitar -1. No final, mostre a média aritmética das notas válidas.
 
-soma = 0
-cont = 0
-nota = 0
+# soma = 0
+# cont = 0
+# nota = 0
 
-while nota != -1:
-    nota = float(input("Digite a nota : "))
-    if nota != -1:
-        soma = soma + nota
-        cont = cont + 1
-        media = soma / cont
-print(f"Media : {media}")
+# while nota != -1:
+#     nota = float(input("Digite a nota : "))
+#     if nota != -1:
+#         soma = soma + nota
+#         cont = cont + 1
+#         media = soma / cont
+# print(f"Media : {media}")
 
 
-# 3) Faça um programa que converta graus Celsius para Fahrenheit. O programa deve continuar convertendo enquanto o usuário não digitar um valor negativo para Celsius.
+# 3) Faça um programa que converta graus Celsius para Fahrenheit. 
+# O programa deve continuar convertendo enquanto o usuário não digitar um valor negativo para Celsius.
+
+# celsius = 0
+# while celsius >= 0:
+#     celsius = float(input("Digite a temperatura em graus Celsius : "))
+#     if celsius >= 0:
+#         fahrenheit = (celsius * 9/5) + 32
+#         print(f"A temperatura em Fahrenheit é {fahrenheit}")
+#     else:
+#         break
+# print("Programa encerrado!")
+
+
+
+#  Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’  
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+while True:
+    sexo = input("Digite o sexo (M/F): ").upper()
+    if sexo == 'M' or sexo == 'F':
+        break
+    else:
+        print("Sexo inválido! Digite M ou F.")   
+
+print(f"Sexo digitado: {sexo}")
